72479ff49565216764cd141eec6660991332dc23
Balázs Gábris <mbair@almaspite.hu>
2023-09-03 13:32:10 +0200

    Add Food Sensitivities to the App

---
+++ src/app/demo/api/food-sensitivity.ts
@@ -0,0 +1,9 @@
+export interface FoodSensitivity {
+    id?: string;
+    szobaszam?: string;
+    szobakod?: string;
+    agyakszama?: string;
+    epulet?: string;
+    agytipus?: string;
+    megjegyzes?: string;
+}
